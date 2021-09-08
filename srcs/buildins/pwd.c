92edf5a09901f5057a3a31a0a170651d54cbc547
bsadie <bsadie@student.21-school.ru>
2021-09-08 11:58:21 +0300

    norm pwd.c

--- srcs/buildins/pwd.c
+++ srcs/buildins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: bsadie <bsadie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 12:18:27 by bsadie            #+#    #+#             */
-/*   Updated: 2021/09/06 13:10:17 by bsadie           ###   ########.fr       */
+/*   Updated: 2021/09/08 11:57:59 by bsadie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_pwd(void)
 {
 	char	*buf;
-	
+
 	buf = NULL;
 	buf = getcwd(buf, 0);
 	if (buf)
