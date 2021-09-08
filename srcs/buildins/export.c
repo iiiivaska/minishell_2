82a3ac9e3a415837fcd8c091de8454b238ddc21b
bsadie <bsadie@student.21-school.ru>
2021-09-08 16:09:21 +0300

    bi memory leak solved

--- srcs/buildins/export.c
+++ srcs/buildins/export.c
@@ -6,7 +6,7 @@
 /*   By: bsadie <bsadie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 11:26:55 by bsadie            #+#    #+#             */
-/*   Updated: 2021/09/08 12:18:11 by bsadie           ###   ########.fr       */
+/*   Updated: 2021/09/08 15:54:22 by bsadie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,26 @@ int	is_key_in_env(t_all *all, char *key)
 	return (1);
 }
 
+int	ft_export_no_arg(t_env *env)
+{
+	t_env	*tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		if (tmp->value && tmp->value[0])
+		{
+			ft_putstr_fd("declare -x ", 1);
+			ft_putstr_fd(tmp->key, 1);
+			ft_putchar('=');
+			ft_putstr_fd(tmp->value, 1);
+			ft_putchar('\n');
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
 int	ft_export(t_all *all, t_list *node)
 {
 	char	**str;
@@ -54,10 +74,12 @@ int	ft_export(t_all *all, t_list *node)
 
 	i = 1;
 	str = node->args;
+	if (!str[i])
+		ft_export_no_arg(all->env_l);
 	while (str[i])
 	{
 		value = get_key(all, str[i]);
-		key = get_value(all, str[i]);
+		key = get_value(all, str[i++]);
 		if (!is_key_in_env(all, key))
 		{
 			replace_value(all->env_l, key, value);
@@ -68,7 +90,6 @@ int	ft_export(t_all *all, t_list *node)
 			ft_lstadd_back_env(&all->env_l, ft_lstnew_env(value, key));
 			ft_lstadd_back_env(&all->hidden_env, ft_lstnew_env(value, key));
 		}
-		i++;
 	}
 	return (0);
 }
